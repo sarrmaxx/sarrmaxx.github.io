c4a159c9b55d4ec6851b41826e0aca8d61460e2e
Maxine <mgs0723@gmail.com>
2019-07-09 15:32:10 -0700

    Initial comit

--- turtles.js
+++ turtles.js
@@ -12,9 +12,9 @@ function originalImg(x){
 
 function myFunction() {
   var x = document.getElementById("text");
-if (x.style.display == "none") {
+  if (x.style.display == "none") {
   x.style.display = "block";
-} else {
-  x.style.display = "block";
-}
+  } else {
+  x.style.display = "none";
+  }
 }
