e1007ae21a0de4edc2792aec93b25d2ec1cb1d2a
gwc <mgs0723@gmail.com>
2019-07-09 14:56:49 -0700

    Initial Commit

---
+++ turtles.js
@@ -0,0 +1,20 @@
+alert("Let's Learn About the Dangers of Plastic in our Oceans");
+
+function bigImg(x){
+   x.style.heigth = "600px";
+   x.style.width = "600px";
+}
+
+function originalImg(x){
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
+
+function myFunction() {
+  var x = document.getElementById("text");
+if (x.style.display == "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "block";
+}
+}
